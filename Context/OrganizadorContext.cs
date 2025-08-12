7bc67aa6b46b8958bc191ad0609164ce3abeb1a6
Diego Bastos <dicabralpersonal@gmail.com>
2025-08-12 16:53:31 -0300

    Alteraçõs concluídas

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -1,14 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Models;
+using System;
 
 namespace TrilhaApiDesafio.Context
 {
     public class OrganizadorContext : DbContext
     {
-        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
-        {
-            
-        }
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options) { }
 
         public DbSet<Tarefa> Tarefas { get; set; }
     }
